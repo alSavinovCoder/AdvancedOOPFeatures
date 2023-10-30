aa4e44ff95e5753b12970a7dabdeb740262c9a9b
Александр Савинов <128948632+alSavinovCoder@users.noreply.github.com>
2023-10-30 21:04:22 +0400

    Add files via upload

---
+++ src/main/java/Main.java
@@ -0,0 +1,31 @@
+import com.skillbox.airport.Airport;
+import com.skillbox.airport.Flight;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+
+    public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
+        //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
+        LocalDateTime currendDateTime = LocalDateTime.now();
+
+        return airport.getTerminals().stream()
+                .flatMap(terminal -> terminal.getFlights().stream())
+                .filter(flight -> flight.getType().equals(Flight.Type.DEPARTURE))
+                .filter(flight -> convertedDateToDateTime(flight).isAfter(currendDateTime)
+                        && convertedDateToDateTime(flight).isBefore(currendDateTime.plusHours(2)))
+                .collect(Collectors.toList());
+    }
+
+    public static LocalDateTime convertedDateToDateTime (Flight date) {
+        return LocalDateTime.ofInstant(date.getDate().toInstant(), ZoneId.systemDefault());
+     }
+
+}
